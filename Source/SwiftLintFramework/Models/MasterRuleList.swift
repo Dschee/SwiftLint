b8e29e6b28cd89d825be0ef62e8ea95b5b37f483
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-07-11 16:40:06 +0200

    Add new VerticalWhitespaceBetweenCases rule with tests

--- Source/SwiftLintFramework/Models/MasterRuleList.swift
+++ Source/SwiftLintFramework/Models/MasterRuleList.swift
@@ -146,6 +146,7 @@ public let masterRuleList = RuleList(rules: [
     ValidIBInspectableRule.self,
     VerticalParameterAlignmentOnCallRule.self,
     VerticalParameterAlignmentRule.self,
+    VerticalWhitespaceBetweenCasesRule.self,
     VerticalWhitespaceRule.self,
     VoidReturnRule.self,
     WeakDelegateRule.self,
