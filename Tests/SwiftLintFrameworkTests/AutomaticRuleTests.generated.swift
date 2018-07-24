83541ed8097bfcd4f71f5ebbc7b9cc6dfee8fc4c
Cihat Gündüz <commits@cihatguenduez.de>
2018-07-24 12:18:22 +0200

    Add tests for different configurations in type_contents_order

--- Tests/SwiftLintFrameworkTests/AutomaticRuleTests.generated.swift
+++ Tests/SwiftLintFrameworkTests/AutomaticRuleTests.generated.swift
@@ -588,12 +588,6 @@ class TypeBodyLengthRuleTests: XCTestCase {
     }
 }
 
-class TypeContentsOrderRuleTests: XCTestCase {
-    func testWithDefaultConfiguration() {
-        verifyRule(TypeContentsOrderRule.description)
-    }
-}
-
 class UnavailableFunctionRuleTests: XCTestCase {
     func testWithDefaultConfiguration() {
         verifyRule(UnavailableFunctionRule.description)
